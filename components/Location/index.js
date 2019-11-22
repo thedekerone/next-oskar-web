c3a089286349fdb44467cad790d880b29018c696
thedekerone <mauriciofow@gmail.com>
2019-11-21 22:05:52 -0500

    maps api

--- components/Location/index.js
+++ components/Location/index.js
@@ -1,38 +1,63 @@
 import React from 'react'
-import { Container, CenterContainer } from '../SharedStyles'
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
-import { LocationContainer, LocationCenterContainer } from './styles'
+import {
+  LocationContainer,
+  LocationCenterContainer,
+  Title,
+  FlexContainer,
+  Description,
+  DescriptionTitle,
+  Text
+} from './styles'
+import { MainButton, AboutButton } from '../Buttons'
 
 export class Location extends React.Component {
   render() {
     return (
-      <LocationContainer color='#f3e2a2'>
+      <LocationContainer color='#C5C4F2'>
         <LocationCenterContainer>
-          <h1> this is a location</h1>
-          <Map
-            google={this.props.google}
-            style={{
-              width: '100%',
-              height: '100%',
-              position: 'relative',
-              maxWidth: '700px',
-              boxSizing: 'border-box'
-            }}
-            initialCenter={{
-              lat: 40.854885,
-              lng: -88.081807
-            }}
-            zoom={15}
-            onClick={this.onMapClicked}
-          >
-            <Marker onClick={this.onMarkerClick} name={'Current location'} />
+          <Title>Location</Title>
+          <FlexContainer>
+            <Map
+              google={this.props.google}
+              style={{
+                width: '100%',
+                height: '400px',
+                position: 'relative',
+                maxWidth: '700px',
+                boxSizing: 'border-box'
+              }}
+              initialCenter={{
+                lat: -9.509646,
+                lng: -77.995859
+              }}
+              zoom={15}
+              className={'maps'}
+              onClick={this.onMapClicked}
+            >
+              <Marker onClick={this.onMarkerClick} name={'Current location'} />
 
-            <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                <h1>aea </h1>
-              </div>
-            </InfoWindow>
-          </Map>
+              <InfoWindow onClose={this.onInfoWindowClose}>
+                <div>
+                  <h1>Yautan</h1>
+                </div>
+              </InfoWindow>
+            </Map>
+            <Description>
+              <DescriptionTitle>Yautan</DescriptionTitle>
+
+              <Text>
+                The Yaután District is one of four belonging to the Province of
+                Casma, in the Department of Ancash in Peru. The capital of the
+                town of Yaután with a population of 3042 inhabitants, is located
+                about 42 km southeast of the city of Casma and 806 meters above
+                sea level. It is the highest district in the province. Yaután is
+                a town hidden among the mountains, full of valleys and varied
+                fruit trees (it is famous producer of avocados and mangoes).
+              </Text>
+              <AboutButton>Learn more</AboutButton>
+            </Description>
+          </FlexContainer>
         </LocationCenterContainer>
       </LocationContainer>
     )
--- components/Location/styles.js
+++ components/Location/styles.js
@@ -4,7 +4,39 @@ import { Container, CenterContainer } from '../SharedStyles'
 export const LocationContainer = styled(Container)`
   position: relative;
 `
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+`
 export const LocationCenterContainer = styled(CenterContainer)`
   position: relative;
-  height: 80vh;
+
+  & .maps {
+    position: relative !important;
+  }
+`
+export const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column-reverse;
+  @media (min-width: 1100px) {
+    align-items: center;
+    flex-direction: row;
+  }
+`
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 4rem;
+  width: 100%;
+  @media (min-width: 1100px) {
+    align-items: flex-end;
+  }
+`
+export const DescriptionTitle = styled.h1`
+  margin-bottom: 1.5rem;
+  font-size: 3rem;
+`
+export const Text = styled.span`
+  text-align: right;
 `
