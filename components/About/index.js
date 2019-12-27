46b996701458da0c4bce1ba8e695039b0da20af2
Mauricio Fow <mauriciofow@gmail.com>
2019-12-27 17:16:19 -0500

    temporary icons

--- components/About/index.js
+++ components/About/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { CardContainer, Title, PlaceHolder } from './styles';
 import { Container, CenterContainer } from '../SharedStyles';
 import { AboutCard } from '../AboutCard';
+
 import { AboutButton } from '../Buttons/index';
 
 export function About() {
@@ -12,16 +13,19 @@ export function About() {
 				<CardContainer>
 					<AboutCard
 						title='Commitment to Customers'
-						description='We make sure that only the best products are delivered to you'
-					/>
+						description='We make sure that only the best products are delivered to you.'>
+						<img src='static/icons/handshake.png' width='100px' alt='' />
+					</AboutCard>
 					<AboutCard
 						title='Teamwork'
-						description='We work together, across boundaries, to meet the needs of our customers and to help our Company win. '
-					/>
+						description='We work together, across boundaries, to meet the needs of our customers and to help our Company win. '>
+						<img src='static/icons/teamwork.png' width='100px' alt='' />
+					</AboutCard>
 					<AboutCard
-						title='Customer Commitment'
-						description='We develop relationships that make a positive difference in our customers lives'
-					/>
+						title='Make a Difference'
+						description='We develop relationships that make a positive difference in our customers lives.'>
+						<img src='static/icons/relas.png' width='100px' alt='' />
+					</AboutCard>
 				</CardContainer>
 
 				{/* Needs Link */}
--- components/AboutCard/index.js
+++ components/AboutCard/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Cards } from '../Cards';
 import { Title, Description, Icon, Container } from './styles';
-import { Agreement } from '../../static/icons';
 
-export function AboutCard({ title = 'What we offer?', description = 'asdasd asads das dasads ads asd ads ad' }) {
+export function AboutCard({ title = 'What we offer?', description = '', children }) {
 	return (
 		<Cards sizeW='22rem'>
 			<Container>
 				<Title>{title}</Title>
 				<Description>{description}</Description>
-				<Icon>
-					<Agreement />
-				</Icon>
+				<Icon>{children}</Icon>
 			</Container>
 		</Cards>
 	);
