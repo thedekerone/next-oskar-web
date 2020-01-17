703eb8636200499a885cbf22e582a54b4fbe5687
thedekerone <mauriciofow@gmail.com>
2020-01-17 18:51:21 -0500

    weglot initialization placed on layout component instead of index page

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Navbar } from '../Navbar';
-import { GlobalStyles } from '../../GlobalStyles';
+import React from 'react'
+import { Navbar } from '../Navbar'
+import { GlobalStyles } from '../../GlobalStyles'
+
+import { useWeglot } from '../../hooks/useWeglot'
 
 export function Layout({ background, color, children, position, fixed }) {
-	return (
-		<div>
-			<GlobalStyles />
-			<Navbar background={background} color={color} fixed={fixed} position={position} />
-			{children}
-		</div>
-	);
+  const [initial] = useWeglot()
+
+  initial !== null &&
+    !initial.initialized &&
+    initial.initialize({
+      api_key: 'wg_91e50a16f59b2922c685b30df90da5677'
+    })
+
+  return (
+    <div>
+      <GlobalStyles />
+      <Navbar
+        background={background}
+        color={color}
+        fixed={fixed}
+        position={position}
+      />
+      {children}
+    </div>
+  )
 }
--- pages/recipes.js
+++ pages/recipes.js
@@ -1,21 +1,12 @@
 // improve code by making it a component
 
-import React, { useState, useEffect } from 'react';
-import { RecipeCard } from '../components/RecipeCard';
-import { Loading } from '../components/Loading';
-import { Layout } from '../components/Layout';
-import styled from 'styled-components';
-import 'isomorphic-fetch';
-import { AboutButton } from '../components/Buttons';
-import { useFetchApi } from '../hooks/useFetchApi';
-import { useLoadMore } from '../hooks/useLoadMore';
-import { RecipePage } from '../components/RecipePage';
-
+import React, { useState, useEffect } from 'react'
+import { RecipePage } from '../components/RecipePage'
 function Dishes(props) {
-	return (
-		<>
-			<RecipePage />
-		</>
-	);
+  return (
+    <>
+      <RecipePage />
+    </>
+  )
 }
-export default Dishes;
+export default Dishes
