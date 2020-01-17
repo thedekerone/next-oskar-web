3956d10d0c0809583c84870f1a84b77f905f75ef
thedekerone <mauriciofow@gmail.com>
2020-01-17 18:21:53 -0500

    testing change color

--- components/Location/index.js
+++ components/Location/index.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import React from 'react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 import {
-	LocationContainer,
-	LocationCenterContainer,
-	Title,
-	FlexContainer,
-	Description,
-	DescriptionTitle,
-	Text
-} from './styles';
-import { MainButton, AboutButton } from '../Buttons';
-import MapGoogle from '../MapGoogle';
+  LocationContainer,
+  LocationCenterContainer,
+  Title,
+  FlexContainer,
+  Description,
+  DescriptionTitle,
+  Text
+} from './styles'
+import { MainButton, AboutButton } from '../Buttons'
+import MapGoogle from '../MapGoogle'
 
 export class Location extends React.Component {
-	render() {
-		return (
-			<LocationContainer color='#C5C4F2'>
-				<LocationCenterContainer>
-					<Title>Location</Title>
-					<FlexContainer>
-						<MapGoogle />
+  render() {
+    return (
+      <LocationContainer color='#2e2d4d'>
+        <LocationCenterContainer>
+          <Title>Location</Title>
+          <FlexContainer>
+            <MapGoogle />
 
-						<Description>
-							<DescriptionTitle>Yautan</DescriptionTitle>
+            <Description>
+              <DescriptionTitle>Yautan</DescriptionTitle>
 
-							<Text>
-								The Yaután District is one of four belonging to the Province of Casma, in the Department
-								of Ancash in Peru. The capital of the town of Yaután with a population of 3042
-								inhabitants, is located about 42 km southeast of the city of Casma and 806 meters above
-								sea level. It is the highest district in the province. Yaután is a town hidden among the
-								mountains, full of valleys and varied fruit trees (it is famous producer of avocados and
-								mangoes).
-							</Text>
-							<AboutButton>Learn more</AboutButton>
-						</Description>
-					</FlexContainer>
-				</LocationCenterContainer>
-			</LocationContainer>
-		);
-	}
+              <Text>
+                The Yaután District is one of four belonging to the Province of
+                Casma, in the Department of Ancash in Peru. The capital of the
+                town of Yaután with a population of 3042 inhabitants, is located
+                about 42 km southeast of the city of Casma and 806 meters above
+                sea level. It is the highest district in the province. Yaután is
+                a town hidden among the mountains, full of valleys and varied
+                fruit trees (it is famous producer of avocados and mangoes).
+              </Text>
+              <AboutButton>Learn more</AboutButton>
+            </Description>
+          </FlexContainer>
+        </LocationCenterContainer>
+      </LocationContainer>
+    )
+  }
 }
--- components/Location/styles.js
+++ components/Location/styles.js
@@ -1,55 +1,58 @@
-import styled from 'styled-components';
-import { Container, CenterContainer } from '../SharedStyles';
+import styled from 'styled-components'
+import { Container, CenterContainer } from '../SharedStyles'
 
 export const LocationContainer = styled(Container)`
   position: relative;
-`;
+`
 export const Title = styled.h1`
-	font-size: 1.5rem;
-	margin-bottom: 2rem;
-`;
+  font-size: 1.5rem;
+  color: #f2f2f2;
+  margin-bottom: 2rem;
+`
 export const LocationCenterContainer = styled(CenterContainer)`
   position: relative;
 
   & .maps {
     position: relative !important;
   }
-`;
+`
 export const FlexContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	flex-direction: column-reverse;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column-reverse;
 
-	/* 100% width for the map and description components*/
-	/* map api can't be customized using styled components */
+  /* 100% width for the map and description components*/
+  /* map api can't be customized using styled components */
 
-	& div {
-		width: 100%;
-	}
+  & div {
+    width: 100%;
+  }
 
-	@media (min-width: 900px) {
-		align-items: center;
-		flex-direction: row;
-	}
-`;
+  @media (min-width: 900px) {
+    align-items: center;
+    flex-direction: row;
+  }
+`
 export const Description = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin-bottom: 5rem;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 5rem;
 
-	@media (min-width: 900px) {
-		align-items: flex-end;
-		margin-left: 9rem;
-		margin-bottom: 0;
-	}
-`;
+  @media (min-width: 900px) {
+    align-items: flex-end;
+    margin-left: 9rem;
+    margin-bottom: 0;
+  }
+`
 export const DescriptionTitle = styled.h1`
-	margin-bottom: 1.5rem;
-	font-size: 3rem;
-`;
+  margin-bottom: 1.5rem;
+  font-size: 3rem;
+  color: #f2f2f2;
+`
 export const Text = styled.span`
-	text-align: left;
-	@media (min-width: 900px) {
-		text-align: right;
-	}
-`;
+  color: #f2f2f2;
+  text-align: left;
+  @media (min-width: 900px) {
+    text-align: right;
+  }
+`
